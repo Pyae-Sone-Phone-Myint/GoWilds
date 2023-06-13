c63fe61e331da306ba51d5203821151a04ab5cf5
Thiha15101051 <at2330227@gmail.com>
2023-06-14 02:14:34 +0630

    tour

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Tours from "../pages/Tours/Tours";
+import TourDetail from "../pages/Tours/TourDetail";
 
 const Path = () => {
+  const [name,setName]=useState("");
   return (
     <>
     
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/tours-page" element={<Tours name={name} setName={setName}/>}/>
+        <Route path="/tours-page/tour-detail" element={<TourDetail name={name}/>}/>
       </Routes>
     </>
   );
